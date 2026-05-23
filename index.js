05991d394528e196e957ab902254bc502b603a7a
jsfindo <johannes.sivola@gmail.com>
2026-05-23 21:36:25 +0300

    gameboard + ships made

---
+++ index.js
@@ -0,0 +1,80 @@
+let gridsize =10
+
+class ship{
+    constructor(shiplength, hitcount =0, sunk = false, position = []){
+        this.shiplength = shiplength
+        this.hitcount = hitcount
+        this.sunk = sunk
+        this.position = position
+
+    }
+
+    hit(){
+        this.hitcount++
+    }
+
+    isSunk(){
+        if(this.hitcount === this.shiplength){
+            this.sunk = true
+        }
+    }
+}
+
+class Gameboard{
+    constructor(grid = [], ships = [], missedAttacks = []){
+        this.grid = grid
+        this.ships = ships
+
+    }
+addShip(startLocation, length, orientation) {
+        const vessel = new ship(length)
+
+        if (orientation == 1) {
+            for (let i = 0; i < length; i++) {
+                if (startLocation[1] + length - 1 > gridsize) {
+                    return "illegal position"
+                }
+                vessel.position.push([startLocation[0], startLocation[1] + i])
+            }
+        }
+
+        if (orientation == 0) {
+            for (let i = 0; i < length; i++) {
+                if (startLocation[0] + length - 1 > gridsize) {
+                    return "illegal position"
+                }
+                vessel.position.push([startLocation[0] + i, startLocation[1]])
+            }
+        }
+
+        this.ships.push(vessel)  // ← fixed
+    }
+
+    receiveAttack(position) {
+        for (const vessel of this.ships) {
+            const hit = vessel.position.some(
+                pos => pos[0] === position[0] && pos[1] === position[1]
+            )
+
+            if (hit) {
+                vessel.hit()
+                vessel.isSunk()
+                return "hit"
+            }
+        }
+
+        this.missedAttacks.push(position)
+        return "miss"
+    }
+
+    gameEndCheck(){
+        if(this.ships.every(vessel => vessel.sunk === true)){
+            return "game over"
+        }
+    }
+
+
+
+}
+
+   
